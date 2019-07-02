8243878c2995a701230b9d61a2576c8d3271d292
Alisson <alisson-henri@live.com>
2019-07-02 17:47:26 -0300

    ajsute

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,13 @@ import { getToken } from "./auth";
 
 const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
+api.postOrPut = (url, id, data, config = {}) => {
+  const method = id ? "put" : "post";
+  const apiUrl = id ? `${url}/${id}` : url;
+
+  return api[method](apiUrl, data, config);
+};
+
 api.interceptors.request.use(async config => {
   const token = getToken();
   if (token) {
